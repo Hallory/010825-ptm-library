bbf1b2ee000f0fa649cd5795c3cedaa13864ad3b
Hallory <khalpeep@gmail.com>
2026-04-01 14:00:48 +0200

    add author filters / api view

--- library/urls/__init__.py
+++ library/urls/__init__.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('categories/', include('library.urls.categories')),
-    path('posts/', include('library.urls.posts'))
+    path('posts/', include('library.urls.posts')),
+    # path('books/', include('library.urls.books')),
+    path('authors/', include('library.urls.authors')),
 ]
